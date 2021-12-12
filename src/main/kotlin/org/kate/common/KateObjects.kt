f4a056475897ee7d2fa726f6812879ad90b45541
keesbroenink <keesbroenink@gmail.com>
2021-12-12 12:10:04 +0100

    Create KateObjects.kt

---
+++ src/main/kotlin/org/kate/common/KateObjects.kt
@@ -0,0 +1,63 @@
+package org.kate.common
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.net.InetAddress
+import java.time.Instant
+import java.util.*
+
+data class KateRequest private constructor(
+    val modelVersion     : String = "1.0",
+    val id               : String,
+    val traceId          : String,
+    val parentRequestId  : String? = null,
+    val replyTopic       : String? = null,
+    val creationTimestamp: Instant = Instant.now(),
+    val requestBodyType  : String, //fully qualified class name
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    var requestBody      : KateRequestBody?
+) {
+    companion object {
+        fun create(id: String=UUID.randomUUID().toString(), traceId: String, parentRequestId: String?=null, replyTopic: String? = null,
+                   requestBody: KateRequestBody) =
+                        KateRequest(id=id, traceId = traceId, parentRequestId = parentRequestId, replyTopic = replyTopic,
+                                    requestBodyType = requestBody.javaClass.canonicalName, requestBody = requestBody)
+    }
+}
+
+data class KateResponse private constructor(
+    val modelVersion     : String = "1.0",
+    val id               : String,
+    val traceId          : String,
+    val creationTimestamp: Instant = Instant.now(),
+    val requestId        : String,
+    val responseBodyType : String, //fully qualified class name
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    var responseBody     : KateResponseBody?
+) {
+    companion object {
+        fun create(id: String=UUID.randomUUID().toString(), traceId: String, requestId: String, responseBody: KateResponseBody) =
+            KateResponse(id=id, traceId = traceId, requestId = requestId,
+                responseBodyType = responseBody.javaClass.canonicalName, responseBody = responseBody)
+    }
+}
+
+data class KateEvent private constructor(
+    val modelVersion     : String = "1.0",
+    val id               : String,
+    val traceId          : String,
+    val creationTimestamp: Instant = Instant.now(),
+    val eventBodyType    : String, //fully qualified class name
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    var eventBody        : KateEventBody?
+) {
+    companion object {
+        fun create(id: String=UUID.randomUUID().toString(), traceId: String, eventBody: KateEventBody) =
+            KateEvent(id=id, traceId = traceId, eventBodyType = eventBody.javaClass.canonicalName, eventBody = eventBody)
+    }
+}
+
+enum class KateErrorImpact{LOW,MEDIUM,HIGH}
+data class KateErrorMessageBody(val impact: KateErrorImpact, val errorDescription: String, val recoveryHint: String,
+                                val relatedRequestId : String? = null, val relatedResponseId : String? = null,
+                                val creationTimestamp: Instant = Instant.now(),
+                                val hostName: InetAddress = InetAddress.getLocalHost()) : KateEventBody
