c8e57de15f74b24f08fd61ecf7f233ef179d0bc2
keesbroenink <keesbroenink@gmail.com>
2021-12-12 12:18:02 +0100

    Create ReflectionUtil.kt

---
+++ src/main/kotlin/org/kate/common/inbound/kafka/ReflectionUtil.kt
@@ -0,0 +1,17 @@
+package org.kate.common.inbound.kafka
+
+import java.lang.reflect.Method
+
+fun getBodyClassname(methods: Array<Method>) : String {
+    methods.forEach {
+        if (it.name == "getBodyType") {
+            it.parameters.forEach {
+                if (it.name == "bodyType") {
+                    val x = it.parameterizedType
+                    return x.typeName
+                }
+            }
+        }
+    }
+    return ""
+}
